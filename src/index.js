86bcb806dcb70cc5e6f982349f50defdb046e1eb
21tarun <tarunuday2000@gmail.com>
2023-02-01 15:54:17 +0530

    project 5 basic structure

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express =require('express')
+const mongoose= require('mongoose')
+const app= express()
+
+
+
+
+
+
+
+app.listen(3000,function(){
+    console.log("server running on ",3000)
+})
