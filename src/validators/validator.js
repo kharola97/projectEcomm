f73ed8c717f5b80595909d80c3cd0515c85c6e87
21tarun <tarunuday2000@gmail.com>
2023-02-01 16:18:45 +0530

    validation module

---
+++ src/validators/validator.js
@@ -0,0 +1,9 @@
+
+const isValid= function(value){
+    if(value==Number) return false
+    if(value==String && value.trim()=="") return false
+    if(!value || value==null) return false
+    return true
+}
+
+module.exports={isValid}
